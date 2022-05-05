3b765e80bb6dee61890097b51843ee6848eb6042
ChihiroHozono <chihiro4434132@gmail.com>
2022-05-05 22:30:44 +0900

    add: play state cls

--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,76 @@ import 'package:simple_logger/simple_logger.dart';
 
 final logger = SimpleLogger();
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
 
-  String sound_path = 'assets/sounds/power2.mp3';
-  String image_path = 'assets/images/muscle_boy1.png';
 
   @override
   Widget build(BuildContext context) {
-    bool isPlay;
-    final player = AudioPlayer();
-    player.setAsset(sound_path,preload: true);
-
     return MaterialApp(
       title: 'MUSCLE BOY',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('MUSCLE BOY'),
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: (){
-              logger.info('on taped');
-              isPlay = player.playing;
-              if (isPlay){
-                logger.info('pause sound');
-                player.pause();
-              }else{
-                logger.info('play sound');
-                player.play();
-              }
+      home: Muscle(),
+    );
+  }
+}
 
-            },
-            child: Image.asset(image_path),
+class Muscle extends StatefulWidget{
+  const Muscle({Key? key}) : super(key: key);
+
+  @override
+  _MuscleState createState() => _MuscleState();
+}
+
+
+class _MuscleState extends State<Muscle> {
+  bool isPlay = false;
+  final player = AudioPlayer();
+  String sound_path = 'assets/sounds/power2.mp3';
+  String image_path = 'assets/images/muscle_boy1.png';
+
+  void _setPlayStatus(){
+    setState(() {
+      isPlay = player.playing;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    player.setAsset(sound_path,preload: true);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('MUSCLE BOY'),
+      ),
+      body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  logger.info('on taped');
+                  _setPlayStatus();
+                  if (isPlay){
+                    logger.info('pause sound');
+                    player.pause();
+                  }else{
+                    logger.info('play sound');
+                    player.play();
+                  }
+
+                },
+                child: Image.asset(image_path),
+              ),
+              Icon(isPlay ? Icons.play_arrow : Icons.pause),
+            ],
           )
-        ),
-      )
+      ),
     );
   }
 }
